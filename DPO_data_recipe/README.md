b8bc2718183ad9a7ae606695b5f6ecafee035ab6
xubuvd <xubuvd@163.com>
2024-08-21 13:40:35 +0800

    updtae

--- DPO_data_recipe/README.md
+++
@@ -1,3 +0,0 @@
-
-
-# Preference Data Recipe
--- data_cleaning/stopall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ps aux|grep wanjuan_clean.py |grep -v grep | awk '{print $2}' | while read pid ;do  kill -9 $pid;done;
-ps aux|grep tokenizer.py |grep -v grep | awk '{print $2}' | while read pid ;do  kill -9 $pid;done;
-ps aux|grep random_sample.py |grep -v grep | awk '{print $2}' | while read pid ;do  kill -9 $pid;done;
